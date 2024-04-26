9db6734dccc80ab8f5e6e9ce2fdc378660717f09
Jesse Guenzl <contact@jesseguenzl.com>
2024-04-26 09:50:00 +0200

    add test endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
