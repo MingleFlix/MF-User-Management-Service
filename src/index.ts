3e782ccd54aa57f57fa51770585c9290b5ddbb13
Jesse Guenzl <contact@jesseguenzl.com>
2024-05-26 16:48:19 +0200

    change port back

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import UserController from './controllers/userController';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
