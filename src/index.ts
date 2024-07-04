d101ed2bba27e1db2ea6c2d02e35d42af72334af
Jesse Guenzl <contact@jesseguenzl.com>
2024-07-05 00:43:36 +0200

    add cookie parser

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from 'express';
 import dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
+import cookieParser from "cookie-parser";
 import swaggerSpec from './swagger';
 import router from "./routes/users";
 
@@ -11,10 +12,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Use cookie-parser middleware
+app.use(cookieParser());
+
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/', router);
 app.use('*', (req: Request, res: Response) => {
-    res.status(404).json({ message: 'user-management: Route not found' });
+    res.status(404).json({message: 'user-management: Route not found'});
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
